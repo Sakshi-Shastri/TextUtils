9ee1d55ac02848158f847bd12080397ec95fc780
Sakshi-Shastri <sakshishastri01@gmail.com>
2026-05-31 06:04:24 +0530

    Change Navbar links from '/' to '#'

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,7 @@ export default function Navbar(props){
         className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}
       >
         <div className="container-fluid">
-          <a className="navbar-brand text-light" href="/">
+          <a className="navbar-brand text-light" href="#">
             {props.title}
           </a>
           <button
@@ -26,7 +26,7 @@ export default function Navbar(props){
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active text-light" aria-current="page" href="/">
+                <a className="nav-link active text-light" aria-current="page" href="#">
                   Home
                 </a>
               </li>
